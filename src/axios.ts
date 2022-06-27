5bb724d48a7404399674b02d6bb130bccf48abad
gbertl <gilbertlcsndle@gmail.com>
2022-06-27 15:28:34 +0800

    change baseURL based on NODE_ENV

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://gilbertlc-api.herokuapp.com/api'
+    : 'http://localhost:8000/api';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL,
 });
 
 export default instance;
