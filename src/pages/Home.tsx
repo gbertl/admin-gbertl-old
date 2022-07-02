977ab3b3a188ceec159ff7763686d99686028875
gbertl <gilbertlcsndle@gmail.com>
2022-07-02 16:06:20 +0800

    utilize useQuery & useMutation in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { Project } from '../typings';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
@@ -6,75 +5,88 @@ import Button from 'react-bootstrap/Button';
 import { generatePath, Link } from 'react-router-dom';
 import routes from '../routes';
 import * as api from '../api';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { Spinner } from 'react-bootstrap';
 
 const Home = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
+  const queryClient = useQueryClient();
+
+  const { isLoading, data: projects } = useQuery<Project[], Error>(
+    'projects',
+    async () => (await api.getProjects(['priority_order'])).data
+  );
+
+  const { mutate: deleteProject } = useMutation(api.deleteProject, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('projects');
+    },
+  });
 
   const handleDelete = async (id: number) => {
     if (!window.confirm('Are you sure want to delete this?')) return;
-    await api.deleteProject(id.toString());
-    setProjects(projects.filter((project) => project.id !== id));
+    deleteProject(id.toString());
   };
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const { data } = await api.getProjects(['priority_order']);
-      setProjects(data);
-    };
-    fetchProjects();
-  }, []);
 
   return (
     <section className="home">
       <Container>
-        <Table responsive="sm" striped>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Priority order</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {projects.map((project) => (
-              <tr key={project.id}>
-                <td>{project.title}</td>
-                <td>{project.priority_order}</td>
-                <td className="d-flex gap-1">
-                  <Link
-                    to={generatePath(routes.projectDetail, {
-                      id: project.id.toString(),
-                    })}
-                    className="btn btn-info"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    to={generatePath(routes.projectEdit, {
-                      id: project.id.toString(),
-                    })}
-                    className="btn btn-warning"
-                  >
-                    Edit
-                  </Link>
-                  <Button
-                    variant="danger"
-                    onClick={() => handleDelete(project.id)}
-                  >
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-            {projects.length === 0 && (
+        {!isLoading ? (
+          <Table responsive="sm" striped>
+            <thead>
               <tr>
-                <td colSpan={3} className="text-center">
-                  No projects found.
-                </td>
+                <th>Title</th>
+                <th>Priority order</th>
+                <th></th>
               </tr>
-            )}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {projects?.length ? (
+                <>
+                  {projects?.map((project) => (
+                    <tr key={project.id}>
+                      <td>{project.title}</td>
+                      <td>{project.priority_order}</td>
+                      <td className="d-flex gap-1">
+                        <Link
+                          to={generatePath(routes.projectDetail, {
+                            id: project.id.toString(),
+                          })}
+                          className="btn btn-info"
+                        >
+                          View
+                        </Link>
+                        <Link
+                          to={generatePath(routes.projectEdit, {
+                            id: project.id.toString(),
+                          })}
+                          className="btn btn-warning"
+                        >
+                          Edit
+                        </Link>
+                        <Button
+                          variant="danger"
+                          onClick={() => handleDelete(project.id)}
+                        >
+                          Delete
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </>
+              ) : (
+                <tr>
+                  <td colSpan={3} className="text-center">
+                    No projects found.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        ) : (
+          <Spinner animation="border" role="status" className="d-block mx-auto">
+            <span className="visually-hidden">Loading ...</span>
+          </Spinner>
+        )}
       </Container>
     </section>
   );
