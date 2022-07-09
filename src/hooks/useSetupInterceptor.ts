09fd80e5760cec356738c3c95dbed1386d760c6a
gbertl <gilbertlcsndle@gmail.com>
2022-07-09 17:21:57 +0800

    remove useffect in setupinterceptor hook

--- src/hooks/useSetupInterceptor.ts
+++ src/hooks/useSetupInterceptor.ts
@@ -1,5 +1,4 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import { axiosPrivate } from '../axios';
 import routes from '../routes';
@@ -9,40 +8,38 @@ const useSetupInterceptor = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  useEffect(() => {
-    axiosPrivate.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const config = error.config;
-        if (error.response.status === 401) {
-          if (location.pathname.includes(routes.login)) return;
+  axiosPrivate.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const config = error.config;
+      if (error.response.status === 401) {
+        if (location.pathname.includes(routes.login)) return;
 
-          const refreshToken = localStorage.getItem('refreshToken');
+        const refreshToken = localStorage.getItem('refreshToken');
 
-          if (refreshToken) {
-            try {
-              const { data } = await api.updateToken(refreshToken);
-              config.headers['Authorization'] = `Bearer ${data.access}`;
-              return axiosPrivate(config);
-            } catch {
-              navigate(routes.login, {
-                state: {
-                  from: location.pathname,
-                },
-              });
-            }
-          } else {
+        if (refreshToken) {
+          try {
+            const { data } = await api.updateToken(refreshToken);
+            config.headers['Authorization'] = `Bearer ${data.access}`;
+            return axiosPrivate(config);
+          } catch {
             navigate(routes.login, {
               state: {
                 from: location.pathname,
               },
             });
           }
+        } else {
+          navigate(routes.login, {
+            state: {
+              from: location.pathname,
+            },
+          });
         }
-        return Promise.reject(error);
       }
-    );
-  }, []);
+      return Promise.reject(error);
+    }
+  );
 };
 
 export default useSetupInterceptor;
