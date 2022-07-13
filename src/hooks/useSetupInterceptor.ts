43ab21efcbc90d4b4511a95cb3cfff4b78a46ddc
gbertl <gilbertlcsndle@gmail.com>
2022-07-13 14:59:27 +0800

    fix useSetupInterceptor to run only once

--- src/hooks/useSetupInterceptor.ts
+++ src/hooks/useSetupInterceptor.ts
@@ -1,45 +1,53 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { axiosPrivate } from '../axios';
+import { useEffect } from 'react';
+
 import routes from '../routes';
 import * as api from '../api';
+import useMounted from './useMounted';
 
 const useSetupInterceptor = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const mounted = useMounted();
 
-  axiosPrivate.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      const config = error.config;
-      if (error.response.status === 401) {
-        if (location.pathname.includes(routes.login)) return;
+  useEffect(() => {
+    if (!mounted) return;
 
-        const refreshToken = localStorage.getItem('refreshToken');
+    axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const config = error.config;
+        if (error.response.status === 401) {
+          if (location.pathname.includes(routes.login)) return;
 
-        if (refreshToken) {
-          try {
-            const { data } = await api.updateToken(refreshToken);
-            config.headers['Authorization'] = `Bearer ${data.access}`;
-            return axiosPrivate(config);
-          } catch {
+          const refreshToken = localStorage.getItem('refreshToken');
+
+          if (refreshToken) {
+            try {
+              const { data } = await api.updateToken(refreshToken);
+              config.headers['Authorization'] = `Bearer ${data.access}`;
+              return axiosPrivate(config);
+            } catch {
+              navigate(routes.login, {
+                state: {
+                  from: location.pathname,
+                },
+              });
+            }
+          } else {
             navigate(routes.login, {
               state: {
                 from: location.pathname,
               },
             });
           }
-        } else {
-          navigate(routes.login, {
-            state: {
-              from: location.pathname,
-            },
-          });
         }
+        return Promise.reject(error);
       }
-      return Promise.reject(error);
-    }
-  );
+    );
+  }, [mounted]);
 };
 
 export default useSetupInterceptor;
